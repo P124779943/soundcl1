e22a006ffffaa1146901eebbbd14845e8564fc39
P124779943 <154750257+P124779943@users.noreply.github.com>
2023-12-26 00:20:22 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = ' https://p124779943.github.io/soundcl1//model.json';
+let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
 
 
 function preload() {
@@ -28,9 +28,7 @@ function setup() {
   createCanvas(320, 240);
   // Start classifying
   // The sound model will continuously listen to the microphone
-  ThunkableWebviewerExtension.postMessage("ready");
   classifier.classify(gotResult);
-  
 }
 
 function draw() {
@@ -52,6 +50,4 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
-  ThunkableWebviewerExtension.postMessage(label);
-
 }
